a895adf3d132da39b717e023f6013363643f98b9
lingchen <ling.146@osu.edu>
2018-03-23 03:09:36 -0400

    map db

--- app/src/main/java/com/example/nbnbzero/nekopapa/MapsActivity.java
+++ app/src/main/java/com/example/nbnbzero/nekopapa/MapsActivity.java
@@ -1,28 +1,25 @@
 package com.example.nbnbzero.nekopapa;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private GoogleApiClient mApiClient;
-    private static final String[] LOCATION_PERMISSIONS = new String[] {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION };
-    private Location mLocation;
-    private LatLng mDefaultLocation;
-    private static final int REQUEST_LOCATION_PERMISSIONS = 0;
-    private boolean mLocationPermissionGranted = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +32,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
     }
 }
--- app/src/main/java/com/example/nbnbzero/nekopapa/RunManager.java
+++ app/src/main/java/com/example/nbnbzero/nekopapa/RunManager.java
@@ -3,7 +3,9 @@ package com.example.nbnbzero.nekopapa;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 
 /**
  * Created by NbnbZero on 3/22/2018.
@@ -16,28 +18,28 @@ public class RunManager {
     private Context mAppContext;
     private LocationManager mLocationManager;
 
-    private RunManager(Context appContext){
+    private RunManager(Context appContext) {
         mAppContext = appContext;
-        mLocationManager = (LocationManager)mAppContext.getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager = (LocationManager) mAppContext.getSystemService(Context.LOCATION_SERVICE);
     }
 
-    public static RunManager get(Context c){
-        if(sRunManager == null){
+    public static RunManager get(Context c) {
+        if (sRunManager == null) {
             sRunManager = new RunManager(c.getApplicationContext());
         }
         return sRunManager;
     }
 
-    private PendingIntent getLocationPendingIntent(boolean shouldCreate){
+    private PendingIntent getLocationPendingIntent(boolean shouldCreate) {
         Intent broadcast = new Intent(ACTION_LOCATION);
-        int flags = shouldCreate? 0 : PendingIntent.FLAG_NO_CREATE;
-        return PendingIntent.getBroadcast(mAppContext,0,broadcast,flags);
+        int flags = shouldCreate ? 0 : PendingIntent.FLAG_NO_CREATE;
+        return PendingIntent.getBroadcast(mAppContext, 0, broadcast, flags);
     }
 
-    public void startLocationUpdates(){
-        String provider  = LocationManager.GPS_PROVIDER;
+    public void startLocationUpdates() {
+        String provider = LocationManager.GPS_PROVIDER;
         PendingIntent pi = getLocationPendingIntent(true);
-        mLocationManager.requestLocationUpdates(provider,0,0,pi);
+        mLocationManager.requestLocationUpdates(provider, 0, 0, pi);
     }
 
     public void stopLocationUpdates(){
