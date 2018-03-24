b8035e369f7bb8a73b6b9669c2b52c60ccf2807e
Zhen Tian <tian.393@osu.edu>
2018-03-24 19:36:25 -0400

    Energy Gauge Works well

--- app/src/main/java/com/example/nbnbzero/nekopapa/GameSessionActivity.java
+++ app/src/main/java/com/example/nbnbzero/nekopapa/GameSessionActivity.java
@@ -1,6 +1,10 @@
 package com.example.nbnbzero.nekopapa;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by NbnbZero and TeriyakiMayo on 2/22/2018.
@@ -9,8 +13,31 @@ import android.support.v4.app.Fragment;
  */
 
 public class GameSessionActivity extends SingleFragmentActivity{
+    static int count = 2;
+    GameSessionFragment frag = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Timer mytime = new Timer();
+        mytime.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                count++;
+                sendDataToFragment();
+            }
+        }, 1 * 5 * 500, 1 * 10 * 500);
+
+    }
+
+    public void sendDataToFragment(){
+        if(frag != null){
+            frag.reduceGauge();
+        }
+    }
+
     @Override
     protected Fragment createFragment() {
-        return new GameSessionFragment();
+        frag = new GameSessionFragment();
+        return frag;
     }
 }
--- app/src/main/java/com/example/nbnbzero/nekopapa/GameSessionFragment.java
+++ app/src/main/java/com/example/nbnbzero/nekopapa/GameSessionFragment.java
@@ -1,14 +1,18 @@
 package com.example.nbnbzero.nekopapa;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.widget.AppCompatImageButton;
+import android.os.Environment;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
-import android.widget.Button;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 /**
  * Created by NbnbZero and TeriyakiMayo on 2/22/2018.
@@ -17,13 +21,27 @@ import android.widget.Button;
  */
 
 public class GameSessionFragment extends Fragment implements View.OnClickListener {
+    AppCompatImageView gaugeView;
+    private Bitmap gaugeBitmap;
+    private String imageFilePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath() +
+            File.separator + "gauge.png";
+    private float perc = 1;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_gamesession, container, false);
 
-        AppCompatImageView loginButton = (AppCompatImageView) v.findViewById(R.id.cat_ear);
-        if (loginButton != null) {
-            loginButton.setOnClickListener(this);
+        AppCompatImageView img = (AppCompatImageView) v.findViewById(R.id.cat_ear);
+        int height = img.getLayoutParams().height;
+    //    img.getLayoutParams().height = height / 2;
+        System.out.println("HHHHHHHHHHHHHHHEIGHT = " + img.getLayoutParams().height);
+        if (img != null) {
+            img.setOnClickListener(this);
         }
+
+        gaugeView = (AppCompatImageView) v.findViewById(R.id.gauge);
+        gaugeBitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.gauge);
+
+        setGauge(perc);
 
         return v;
     }
@@ -32,7 +50,38 @@ public class GameSessionFragment extends Fragment implements View.OnClickListene
         switch (view.getId()) {
             case R.id.cat_ear:
                 System.out.println("TTTTTTTTTTTTTTTTTT");
+         //       perc -= .1f;
+         //       setGauge(perc);
                 break;
         }
+    }
+
+    private void setGauge(float perc){
+        Matrix matrix = new Matrix();
+        matrix.postScale(perc, 1f);
+
+        int width = gaugeView.getLayoutParams().width;
+        int gaugeHeight = gaugeView.getLayoutParams().height;
+
+        Bitmap newBitmap = Bitmap.createBitmap(gaugeBitmap, 0, 0, width, gaugeHeight, matrix, true);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        newBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+        gaugeView.setImageBitmap(newBitmap);
+    }
+
+    public void reduceGauge(){
+        getActivity().runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                System.out.println("GAUGE UPDATED=================");
+                if(perc >= .1f) {
+                    perc -= .1f;
+                }
+                setGauge(perc);
+            }
+        });
+
     }
 }
