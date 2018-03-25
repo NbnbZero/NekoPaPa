786ddd0582b62e0947301e3898af0885ce67ae9b
lingchen <ling.146@osu.edu>
2018-03-25 18:16:47 -0400

    map db

--- app/src/main/java/com/example/nbnbzero/nekopapa/MapsActivity.java
+++ app/src/main/java/com/example/nbnbzero/nekopapa/MapsActivity.java
@@ -2,32 +2,43 @@ package com.example.nbnbzero.nekopapa;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.widget.Toast;
-
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
-
+    private GoogleApiClient mGoogleApiClient;
+    private GPSTracker gpsTracker;
+    private Location mLocation;
+    double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+       gpsTracker = new GPSTracker(getApplicationContext());
+       mLocation = gpsTracker.getLocation();
+
+        latitude = mLocation.getLatitude();
+        longitude = mLocation.getLongitude();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
     }
@@ -35,9 +46,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        }
+        LatLng myloc = new LatLng(latitude,longitude);
+        mMap.addMarker(new MarkerOptions().position(myloc).title("I'm here..."));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myloc));
     }
+
+    public void onStart(){
+        super.onStart();
+    }
+    public void onStop(){
+        super.onStop();
+    }
+    public void onPause(){
+        super.onPause();
+    }
+    public void onResume(){
+        super.onResume();
+    }
+
+    public void onLocationChanged(Location location){
+
+    }
+
+    public void onConnectionSuspended(int arg0){
+
+    }
+
+    public void onStatusChange(String provider, int status, Bundle extras){
+
+    }
+
+
+    public void onConnected(Bundle args0){
+
+    }
+
+    public void onConnectionFailed(ConnectionResult result){
+
+    }
+
+
 }
